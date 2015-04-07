618ee49987ef6c10525c8b10af6657fc9f709eef
Paola <Paola@10.68.174.201>
2015-04-07 09:01:52 -0500

    Se realiza una actualización al proyecto SIBDI

---
+++ src/controlador/ControlBusqueda.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import vista.Reporte;
+
+/**
+ *
+ * @author Paola
+ */
+public class ControlBusqueda implements ActionListener {
+    
+    private Reporte repor;
+    
+    public ControlBusqueda(Reporte repor){
+         this.repor= repor;
+        
+    }
+    
+ 
+    public void actionPerformed(ActionEvent evento) {
+        
+    }
+        
+    
+}//fin de la clase 
